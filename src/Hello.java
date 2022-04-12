7cbbe7f1e18a02743fb9f677b17a0e1a2da22e42
oswaldo garcia <garcia.oswaldo1000@gmail.com>
2022-04-12 15:36:01 -0500

    first update

---
+++ src/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello My name is Ozzy");
+        int myFirstNumber= (10+5)+(2*10);
+        int mySecondNumber=12;
+        int myThirdNumber=myFirstNumber*2;
+        int myTotal=myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne=1000-myTotal;
+        System.out.println(myTotal);
+        System.out.println("this is my first number"+" "+myFirstNumber);
+        System.out.println("This is my last one after subtracting my total"+" "+ myLastOne);
+    }
+}
